e33825689819ac2c2f760faf653c9b15d0edacea
rajat-saxena14 <49250295+rajat-saxena14@users.noreply.github.com>
2021-06-02 21:55:56 +0530

    initial commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2019 Online Library Management System |<h1> Designed and Managed by : Rajat Saxena</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
