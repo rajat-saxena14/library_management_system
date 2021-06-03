74a3c32c4fbc0012df12a91810dab3934820798f
rajat-saxena14 <49250295+rajat-saxena14@users.noreply.github.com>
2021-06-03 20:41:25 +0530

    db config update

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,6 @@
 <?php 
 //Get Heroku ClearDB connection information
+/*
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
@@ -7,25 +8,25 @@ $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
+*/
 
 // DB credentials.
-/*define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','library');*/
+define('DB_HOST','us-cdbr-east-04.cleardb.com');
+define('DB_USER','b98a1a17c27d41');
+define('DB_PASS','77bb524d');
+define('DB_NAME','heroku_167a85b8cf4de48');*/
 // Establish database connection.
 
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-/*
+
 try
 {
-    //$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
 catch (PDOException $e)
 {
 exit("Error: " . $e->getMessage());
 }
 ?>
-*/
