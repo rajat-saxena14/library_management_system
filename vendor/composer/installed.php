7c263adaba2a12fc709f4341d6f79eaa814d0130
rajat-saxena14 <49250295+rajat-saxena14@users.noreply.github.com>
2021-06-03 20:18:26 +0530

    associated vendor files and composer.lock updated

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd5b8528668b6874efaa3ccee1ae240c40ae64225',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd5b8528668b6874efaa3ccee1ae240c40ae64225',
+    ),
+  ),
+);
